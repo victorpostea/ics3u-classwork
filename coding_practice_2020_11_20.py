b212df81d156939925c86a392485351d48f91ed6
victorpostea <60620574+victorpostea@users.noreply.github.com>
2020-11-20 12:51:19 -0500

    Create coding_practice_2020_11_20.py

---
+++ coding_practice_2020_11_20.py
@@ -0,0 +1,3 @@
+favcolor = input("What's your favorite color?:")
+favcolorcaps = favcolor.capitalize()
+print(f"{favcolorcaps}? ME TOO! I really love {favcolor} infact a wall in my room is painted {favcolor}.")
