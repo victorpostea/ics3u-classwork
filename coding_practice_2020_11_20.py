91928c5313d1d568c83a05846dcb45e0df84f4b6
victorpostea <60620574+victorpostea@users.noreply.github.com>
2020-11-20 13:24:20 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -12,3 +12,26 @@ packs = int(input("How many packs are there?:"))
 totalcans = packs * cans_per_pack
 print(f"There is a total of {totalcans} cans.")
 
+
+#3
+
+width = int(input("What is the width?:"))
+length = int(input("What is the length?:"))
+height = int(input("What is the height?:"))
+volume = height * length * width
+
+print(f"The volume is {volume} units^3.")
+
+
+#4
+
+choice = input("If you join this google meet will you mute the teacher?:")
+choicelower = choice.lower()
+if choicelower == "no":
+    print("Ok good.")
+elif choicelower == "yes":
+    print("That's probably not a great idea.")
+else:
+    print("error")
+    
+    
