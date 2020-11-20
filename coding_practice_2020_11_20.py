bd0b48d96b523a717bbad0e4baa2f2cfb05b8923
victorpostea <60620574+victorpostea@users.noreply.github.com>
2020-11-20 13:08:22 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,3 +1,13 @@
+# Q1
+
 favcolor = input("What's your favorite color?:")
 favcolorcaps = favcolor.capitalize()
 print(f"{favcolorcaps}? ME TOO! I really love {favcolor} infact a wall in my room is painted {favcolor}.")
+
+
+# Q2
+
+cans_per_pack = int(input("How many cans are there in a pack?:"))
+packs = int(input("How many packs are there?:"))
+print(packs * cans_per_pack)
+
